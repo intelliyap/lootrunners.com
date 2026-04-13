9b4926385f7ff75362ac840d80a6e8e1c357bced
Daniel Oh <doh@rodeo.local>
2026-04-12 21:26:29 -0500

    Remove unused lootrunners-website.png import

--- components/programs/Welcome.tsx
+++ components/programs/Welcome.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./Welcome.module.css";
-import lootrunners from "../../public/lootrunners-website.png";
 import check from "@/components/assets/check.png";
 import { SettingsLink } from "../SettingsLink";
 import history from "./updateAssets/history.png";
